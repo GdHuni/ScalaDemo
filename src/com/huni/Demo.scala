efb5f8731e2e31d6a79f7adf4c4b36e8ee42ceeb
周虎 <zhouhu@leyoujia.com>
2021-04-26 16:18:28 +0800

    scala

---
+++ src/com/huni/Demo.scala
@@ -0,0 +1,124 @@
+package com.huni
+
+
+
+/**
+ * @功能描述:
+ * @项目版本: 1.0.0
+ * @项目名称: 大数据数据接入平台
+ * @相对路径: com.huni.Demo
+ * @author: <a href="mailto:zhouhu@leyoujia.com">周虎</a>
+ * @创建日期: 2021/3/13 0013 10:12
+ */
+object Demo {
+  def main(args: Array[String]): Unit = {
+/*    print("aa");
+    println("ss");
+    var y = 0
+    val x = y = 1
+    print("x:"+x)*/
+/*
+    val subject = "Spark"
+    val str1 = s"Hello, $subject"
+    println(str1)
+
+    println("=========================")
+
+    val arr = (1 to 10).toArray
+    val str2 = s"arr.length = ${arr.length}"
+    println(str2)
+    println(s"The answer is ${6*6}")
+
+    println("=========================")
+
+    val year=2020
+    val month=6
+    val day=9
+    println(s"$year-$month-$day")
+    println(f"$year-$month%02d-$day%02d")
+
+    println("a\nb\tc")
+    println(raw"a\nb\tc")
+    println("""a\nb\tc""")
+*/
+
+    println("=========================")
+
+    /**
+     * 块表达式
+     */
+    val s = {
+      val a =3;
+      val b =4;
+      a+b
+    }
+    val year=2020
+    val month=6
+    val day=9
+    /**
+     * 插值器
+     */
+    println("s"+s);
+    println(s"s+$s");
+    println(f"$year-$month%02d-$day%02d")
+    println(raw"$s \n");
+
+    /**
+     * if else
+     */
+    val a = if(1>2){
+      1
+    }  else{
+      ()
+    }
+    println("a: "+a)
+
+    /**
+     * for 循环
+     */
+
+      for (i <- 1 until 4; j <- 2 until 4){
+        println(s"i * j = $i * $j = ${i * j}")
+        println(i * j )
+      }
+
+      for (i <- 1 to 10; j <- 1 to 10 if i==j){
+        println(s"i * j = $i * $j = ${i * j}")
+      }
+    //守卫语句。增加 if 条件语句
+    for(i <- 1 to 10 if i<3){
+      println(s"守卫语句: $i")
+    }
+    // 使用 yield 接收返回的结果，这种形式被称为for推导式
+    val result = for(i <- 1 to 10 ) yield i
+    print("result: "+ result )
+    for (i <- 0 to result.length-1){
+      print( i+"\t")
+    }
+
+    /** break 关键字*/
+    import util.control.Breaks._
+    for(i<-0 until 10){
+      breakable{
+        if(i==3||i==6) {
+          break()
+        }
+        println(i)
+      }
+    }
+
+    println("函数： "+myFunction(1,2))
+    println("函数： "+myFunction2(1,2))
+  }
+
+  /**函数*/
+  def myFunction(a:Int,b:Int):Int = {
+    a+b
+  }
+  /**函数*/
+  def myFunction2(a:Int,b:Int):Unit = {
+    a+b
+  }
+
+
+}
---
+++ src/com/huni/FileDemo.scala
@@ -0,0 +1,70 @@
+package com.huni
+import java.io.{PrintWriter, Writer}
+
+import scala.io.Source
+import scala.io.BufferedSource
+/**
+ * @功能描述:
+ * @项目版本: 1.0.0
+ * @项目名称: 大数据数据接入平台
+ * @相对路径: com.huni.FileDemo
+ * @author: <a href="mailto:zhouhu@leyoujia.com">周虎</a>
+ * @创建日期: 2021/4/26 0026 15:33
+ */
+object FileDemo {
+  def main(args: Array[String]): Unit = {
+    readTextFile
+    readUrl
+    writerTextFile
+
+  }
+
+  /**
+   * Scala没有内建的对写入文件的支持。要写入文本文件，可使用 java.io.PrintWriter
+   */
+  def writerTextFile(): Unit={
+    val writer = new PrintWriter("src\\t.text")
+    for(i <- 1 to 100){
+      writer.println(i)
+      writer.flush()
+    }
+    writer.close()
+
+  }
+  /**
+   * 讀取網絡資源
+   */
+  def readUrl(): Unit={
+    val source = Source.fromURL("http://www.baidu.com")
+
+    val strings = source.getLines()
+    for (s <- strings){
+      println(s)
+    }
+    println("=========================")
+    val string = source.mkString
+    println(string)
+
+  }
+  /**
+   * 讀取文件
+   */
+  def readTextFile(): Unit={
+    /**文件读取*/
+    val file = Source.fromFile("src\\t.text")
+    val lines: Iterator[String] = file.getLines()
+    for (line <- lines) {
+      println(line)
+    }
+
+    val source = Source.fromFile("F:\\1哈.txt")
+    val strings = source.getLines()
+    for(s <- strings){
+      println("messgae: "+s)
+    }
+
+    //关闭文件
+    file.close()
+    source.close()
+  }
+}
