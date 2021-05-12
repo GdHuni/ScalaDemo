4ea372be906830d4fcf759b57ce894548b23e869
周虎 <zhouhu@leyoujia.com>
2021-05-12 10:26:19 +0800

    scala高阶函数（数组操作）

--- src/com/huni/OperatorArrayDemo.scala
+++ src/com/huni/OperatorArrayDemo.scala
@@ -1,4 +1,5 @@
 package com.huni
+import scala.collection.mutable
 
 /**
  * @功能描述: 操作数据
@@ -57,6 +58,72 @@ object OperatorArrayDemo {
     println(nums2.dropRight(3).toBuffer)
     //从左向右删除符合条件的元素，如果条件不成立，就终止删除操作
     println(nums2.dropWhile(_ < 4).toBuffer)
+
+
+
+    println("========================================")
+    //将数组分为两部分，前N个为一部分，剩下的为另一部分 splitAt 底层就是take(n) drop(n)
+    val tuple1: (Array[Int], Array[Int]) = nums2.splitAt(4)
+
+    val tuple: (Array[Int], Array[Int]) = nums2.splitAt(3)
+    println(tuple._1.toBuffer + "  " + tuple._2.toBuffer)
+    //对数组进行切片操作，取出从索引2到索引4的元素，不包括索引为5的元素
+    println(nums2.slice(2, 5).toBuffer)
+
+
+    println("========================================")
+    val array1 = Array("A", "B", "C")
+    val array2 = Array(1, 2, 3, 4)
+    //拉链操作，当两个数组的长度不一样时，截取相同的长度
+    val z1: Array[(String, Int)] = array1.zip(array2)
+    println(z1.toBuffer)
+    //拉链操作，当两个数组长度不一样时，array1用*填充，array2用-1填充
+    val z2 = array1.zipAll(array2, "*", -1)
+    //拉链操作，当两个数组长度不一样时，array2用*填充，array1用-1填充
+    val z3 = array2.zipAll(array1, "*", -1)
+    //用数组索引进行填充
+    val z4 = array1.zipWithIndex
+    println(z2.toBuffer)
+    println(z3.toBuffer)
+    println(z4.toBuffer)
+
+    println("========================================")
+    //通过unzip进行拆分数组的操作
+    // 通过unzip把z4拆分成两个数组
+    val (l1, l2) = z4.unzip
+    println(l1.toBuffer)
+    println(l2.toBuffer)
+
+    val (l3, l4, l5) = Array((1, "one", '1'), (2, "two", '2'), (3, "three", '3')).unzip3
+    println(l3.toBuffer)
+    println(l4.toBuffer)
+    println(l5.toBuffer)
+
+    //数组的操作符：:+  +:  ++
+    // :+用于在数组的尾部追加元素； +：用于在数组的头部追加元素
+    // ++用于连接两个集合（比如：数组、列表等）
+
+    val n1: Array[Int] = (1 to 4 ).toArray
+
+    val n2: mutable.Buffer[Int] = (1 to 4 ).toBuffer
+    n2.append(3)
+
+    val nums3 = n1 :+ 10
+    val nums4 = 10 +: n2
+    println("========================================")
+    println(nums3.toBuffer)
+    println(nums4)
+
+    println("===========数组排序==============")
+    val sortNums=Array(1,3,5,2,7,8,6,9)
+    //升序
+    println(sortNums.sorted.toBuffer)
+    println(sortNums.sortWith(_<_).toBuffer)
+    //降序
+    println(sortNums.sorted.reverse.toBuffer)
+    println(sortNums.sorted.toBuffer.reverse)
+    println(sortNums.sortWith(_>_).toBuffer)
+
   }
 
 }
